12ce02ea53004f4b9d6790b08ffd563e057ed422
helvayne-br <paulornjunior.89@gmail.com>
2022-02-16 20:51:36 -0300

    acerto metodo "void".

--- Projeto-java/src/MinhaClasse.java
+++ Projeto-java/src/MinhaClasse.java
@@ -1,5 +1,7 @@
 public class MinhaClasse {
-    public static void public static void main(String[] abacaxi) {
+    public static void main(String[] abacaxi) {
+        
+    } {
         System.out.print("meu primeiro código");
         
     }
