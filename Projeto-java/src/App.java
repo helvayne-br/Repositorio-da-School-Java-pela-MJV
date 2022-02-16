52e6f656d2d3c48ceffc268db2673b45ad07bc1e
helvayne-br <paulornjunior.89@gmail.com>
2022-02-16 20:47:39 -0300

    atualizacao

--- Projeto-java/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Gleysson!!!  To na MJV School");
-    }
-}
---
+++ Projeto-java/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void public static void main(String[] abacaxi) {
+        System.out.print("meu primeiro código");
+        
+    }
+}
