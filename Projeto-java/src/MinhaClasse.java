dfbc52950f565791acff0e335ab6e6ef11f0fa1b
helvayne-br <paulornjunior.89@gmail.com>
2022-02-16 21:50:52 -0300

    Atualização #2

--- Projeto-java/src/MinhaClasse.java
+++ Projeto-java/src/MinhaClasse.java
@@ -1,8 +1,17 @@
 public class MinhaClasse {
-    public static void main(String[] abacaxi) {
+    public static void main(String [] args) {
+        int resultado = MinhaClasse.somar(3,7);
+        System.out.println(args[0]);
+
+        if(args.length == 2)
+            System.out.println(args[1]);
         
-    } {
-        System.out.print("meu primeiro código");
+            System.out.println(resultado);    
+    } 
+        static int somar (int numeroUm, int numeroDois){
+            int resultado = numeroDois + numeroUm;
+            return resultado;
+        }
         
     }
-}
+
